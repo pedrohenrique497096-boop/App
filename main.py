3a52bdca895a34e633b438b7e7a737a7596374f6
Xxx <pedrohenrique497096@gmail.com>
2026-04-01 18:43:18 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 from vision.analyzer import analisar
 from control.actions import executar
-from generate_tv_script import gerar_script
+from visual.chart import desenhar
 
 dados = analisar()
 executar(dados)
-
-if dados.get("direcao") != "NEUTRO":
-    gerar_script(dados)
+desenhar(dados)
