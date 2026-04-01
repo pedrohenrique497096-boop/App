416277846f78159784917571589d0ca890faa3b2
Xxx <pedrohenrique497096@gmail.com>
2026-04-01 15:01:47 -0300

    Update analyzer.py

--- vision/analyzer.py
+++ vision/analyzer.py
@@ -1,10 +1,20 @@
 def analisar():
-    print("Analisando gráfico...")
+    print("Analisando estrutura...")
 
-    # lógica futura (SMC, estrutura, etc)
+    # SIMULAÇÃO (depois vamos pegar do gráfico)
+    tendencia = "ALTA"
+    ultimo_topo = 2050
+    ultimo_fundo = 2000
+    preco_atual = 2030
+
+    if tendencia == "ALTA" and preco_atual > ultimo_fundo:
+        direcao = "BUY"
+    else:
+        direcao = "SELL"
+
     return {
-        "direcao": "BUY",
-        "entrada": 0,
-        "stop": 0,
-        "tp": 0
+        "direcao": direcao,
+        "entrada": preco_atual,
+        "stop": ultimo_fundo,
+        "tp": ultimo_topo
     }
